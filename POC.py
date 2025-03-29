b5d00727b3eed140cc66741c43d86b235b5bee02
Kha Nguyen <kha@sellforte.com>
2025-03-29 15:39:34 +0700

    next generation
    version

--- POC.py
+++ POC.py
@@ -1,157 +1,106 @@
-import hmac
 import os
-from dataclasses import dataclass
+from typing import List, Self
 
-import nest_asyncio
 import streamlit as st
-from google.cloud import texttospeech as tts, vision
-from pydantic import BaseModel, Field
-from pydantic_ai import Agent, RunContext
-from pydantic_ai.models.gemini import GeminiModel
+from pydantic import BaseModel, Field, model_validator
+from pydantic_ai import Agent, BinaryContent
+from google.cloud import texttospeech as tts
+from PIL import Image, ImageDraw
+import nest_asyncio
 
 nest_asyncio.apply()
 
 st.title("Mimir POC")
-
 GCP_API_KEY = os.getenv("GCP_API_KEY")
 
-if GCP_API_KEY is None:
-    st.error("API_KEY not set")
-    st.stop()
 
-
-def check_password():
-    """Returns `True` if the user had the correct password."""
-
-    def password_entered():
-        """Checks whether a password entered by the user is correct."""
-        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
-            st.session_state["password_correct"] = True
-            del st.session_state["password"]  # Don't store the password.
-        else:
-            st.session_state["password_correct"] = False
-
-    # Return True if the password is validated.
-    if st.session_state.get("password_correct", False):
-        return True
-
-    # Show input for password.
-    st.text_input(
-        "Password", type="password", on_change=password_entered, key="password"
+class BoundingBox(BaseModel):
+    x0: float = Field(
+        ..., description="X coordinate of the top-left corner", ge=0.0, le=1.0
     )
-    if "password_correct" in st.session_state:
-        st.error("😕 Password incorrect")
-    return False
+    y0: float = Field(
+        ..., description="Y coordinate of the top-left corner", ge=0.0, le=1.0
+    )
+    x1: float = Field(
+        ..., description="X coordinate of the bottom-right corner", ge=0.0, le=1.0
+    )
+    y1: float = Field(
+        ..., description="Y coordinate of the bottom-right corner", ge=0.0, le=1.0
+    )
+
+    @model_validator(mode="after")
+    def check_coordinates(self) -> Self:
+        if self.x0 >= self.x1 or self.y0 >= self.y1:
+            raise ValueError("Invalid bounding box coordinates")
+        return self
+
+    def scale_coordinate(self, w_scale: int = 1, h_scale: int = 1) -> List[int]:
+        return [
+            int(self.x0 * w_scale),
+            int(self.y0 * h_scale),
+            int(self.x1 * w_scale),
+            int(self.y1 * h_scale),
+        ]
 
 
-if not check_password():
-    st.stop()
+class Narative(BaseModel):
+    landmark: str = Field(..., description="Landmark name")
+    bounding_box: BoundingBox = Field(..., description="Bounding box coordinates")
+    story: str = Field(..., description="A brief history of the landmark")
 
-__LANGUAGES__ = ["English", "Finnish", "Swedish", "Vietnamese", "Japanese"]
-__LANGUAGE_CODES__ = {
-    "English": "en-US",
-    "Finnish": "fi-FI",
-    "Swedish": "sv-SE",
-    "Vietnamese": "vi-VN",
-    "Japanese": "ja-JP",
-}
 
-image_file = st.file_uploader(
-    label="Upload image",
-    type=["jpg", "png", "jpeg"],
+class Naratives(BaseModel):
+    naratives: List[Narative] = Field(..., description="A list of detected landmarks")
+
+
+geo_agent = Agent(
+    model="google-gla:gemini-2.0-flash",
+    result_type=Naratives,
+    system_prompt="""
+    You are a geolocation expert.
+    Your task is to analyze the image and provide a list of landmarks with the coordinates
+    of the bounding boxes where the landmarks are,
+    and a brief history of each landmark.
+    The bounding boxes should be as close as possible to the landmarks.
+    The coordinates should be in the format [x0, y0, x1, y1], where (x0, y0) is the top-left corner,
+    and (x1, y1) is the bottom-right corner. The coordinates should be normalized to the range [0, 1].
+    The story should be upbeat, like a funny tour guide. Keep the story between 200 to 500 words.
+    There should be details about the history, but not boring like a lecture.
+    """,
 )
 
+tts_client = tts.TextToSpeechClient(client_options={"api_key": GCP_API_KEY})
 
-@dataclass
-class NarrativeFeature:
-    landmark: str
-    language: str
+image = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
-
-class Narrative(BaseModel):
-    story: str = Field(description="The story about the landmark")
-
-
-def get_agent(api_key: str):
-    model = GeminiModel("gemini-2.0-flash-exp", api_key=api_key)
-    agent = Agent(
-        model=model,
-        deps_type=NarrativeFeature,
-        result_type=Narrative,
-        system_prompt="""
-        You are a tour guide. You are telling the user about the history of the given landmark.
-        You should tell the history in the manner of a story, and you can mix in a little bit of fiction.
-        The goal is to teach the user about some history, while keep it entertaining.
-        Keep the story short.""",
+if image is not None:
+    result = geo_agent.run_sync(
+        [
+            "Tell me about the landmarks in this image. Locate the bounding boxes of the landmarks.",
+            BinaryContent(image.read(), media_type="image/png"),
+        ]
     )
 
-    @agent.system_prompt
-    def get_landmark(ctx: RunContext[NarrativeFeature]) -> str:
-        return f"The landmark is {ctx.deps.landmark!r}."
+    with Image.open(image) as pil_image:
+        canvas = ImageDraw.Draw(pil_image)
+        w, h = pil_image.width, pil_image.height
 
-    @agent.system_prompt
-    def get_language(ctx: RunContext[NarrativeFeature]) -> str:
-        return f"The story should be told in {ctx.deps.language!r}."
+        for landmark in result.data.naratives:
+            box = landmark.bounding_box.scale_coordinate(w, h)
+            canvas.rectangle(box, outline="red", width=5)
 
-    return agent
+        st.image(pil_image)
 
+    for i, landmark in enumerate(result.data.naratives):
+        st.write(landmark.landmark)
+        st.write(landmark.story)
 
-def main(api_key: str):
-    if image_file is not None:
-        vision_client = vision.ImageAnnotatorClient(client_options={"api_key": api_key})
-        tts_client = tts.TextToSpeechClient(client_options={"api_key": api_key})
-
-        image = vision.Image(content=image_file.read())
-        response: vision.AnnotateImageResponse = vision_client.landmark_detection(image=image)
-
-        st.image(image_file, use_container_width=True)
-
-        for landmark in response.landmark_annotations:
-            message = f"""
-            ### {landmark.description} ({landmark.score:.3f})\n
-
-            ({landmark.locations[0].lat_lng.latitude:.4f}, {landmark.locations[0].lat_lng.longitude:.4f})
-
-            ---
-            """
-
-            st.markdown(message)
-
-        if len(response.landmark_annotations) >= 1:
-            agent = get_agent(api_key)
-            st.warning("Select the values below slowly between selections")
-            landmark = st.selectbox(
-                "Landmark",
-                [landmark.description for landmark in response.landmark_annotations],
-            )
-            language = st.selectbox(
-                "Language", __LANGUAGES__
-            )
-
-            deps = NarrativeFeature(
-                landmark=landmark,
-                language=language,
-            )
-            result = agent.run_sync("Tell me about this landmark.", deps=deps)
-            voices = tts_client.list_voices(language_code=__LANGUAGE_CODES__[language])
-            voice_name = st.selectbox("Voice", [voice.name for voice in voices.voices])
-
-            st.write(result.data.story)
-
-            tts_response = tts_client.synthesize_speech(
-                input=tts.SynthesisInput(text=result.data.story),
-                voice=tts.VoiceSelectionParams(
-                    language_code=__LANGUAGE_CODES__[language],
-                    name=voice_name,
-                ),
-                audio_config=tts.AudioConfig(audio_encoding=tts.AudioEncoding.LINEAR16)
-            )
-            st.audio(
-                data=tts_response.audio_content,
-                format="audio/wav",
-                autoplay=True
-            )
-
-
-if __name__ == "__main__":
-    main(GCP_API_KEY)
+        tts_response = tts_client.synthesize_speech(
+            input=tts.SynthesisInput(text=landmark.story),
+            voice=tts.VoiceSelectionParams(
+                language_code="en-US",
+                name="en-US-Chirp3-HD-Aoede",
+            ),
+            audio_config=tts.AudioConfig(audio_encoding=tts.AudioEncoding.LINEAR16),
+        )
+        st.audio(data=tts_response.audio_content, format="audio/wav", autoplay=i == 0)
